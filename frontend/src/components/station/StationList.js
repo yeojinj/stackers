df3a40d4837ba98e675b321ce23e09abd3eabaff
cjswp3550 <cjswp3550@naver.com>
2023-01-30 16:55:18 +0900

    Style: 메인페이지 레이아웃 구성 수정

---
+++ frontend/src/components/station/StationList.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import StationListItem from './StationListItem'
+
+function StationList() {
+  return (
+    // 15개의 StationListItem 만들기
+    <StationListItem />
+  )
+}
+
+export default StationList
