b0e2a43482b14a8a9fde22306cda85c46c59f477
limse0204 <limse0204@naver.com>
2023-01-30 14:12:23 +0900

    Feat: 스테이션 엔티티 추가

---
+++ backend/src/main/java/com/ssafy/stackers/model/Station.java
@@ -0,0 +1,53 @@
+package com.ssafy.stackers.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
+import java.time.LocalDateTime;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.ColumnDefault;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+public class Station {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private int id;
+
+    @Column(name = "content", length = 300, nullable = false)
+    private String content;
+
+    @Column(name = "reg_time", nullable = false)
+    private LocalDateTime regTime;
+
+    @Column(name = "music")
+    private String music;
+
+    @Column(name = "heart_cnt")
+    @ColumnDefault("0")
+    private int heartCnt;
+
+    @Column(name = "remain_depth")
+    @ColumnDefault("4")
+    private int remainDepth;
+    @Column(name = "is_public")
+    private boolean isPublic;
+    @Column(name = "is_complete")
+    private boolean isComplete;
+    @Column(name = "is_delete")
+    private boolean isDelete;
+    
+    @OneToOne           // 스테이션 당 무조건 1개
+    private Member member;
+
+    @OneToOne           // Video는 스테이션 당 무조건 1개
+    private Video video;
+}
