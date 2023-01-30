cc26e5ff605d6287ae1849dc18441487ceef09a9
sbsc20 <sbsc20@naver.com>
2023-01-30 17:52:38 +0900

    Feat: 패스워드 인코더 추가

--- backend/src/main/java/com/ssafy/stackers/StackersApplication.java
+++ backend/src/main/java/com/ssafy/stackers/StackersApplication.java
@@ -2,11 +2,16 @@ package com.ssafy.stackers;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
-// TODO: exclude 부분 DB 연결 후 삭제할 것
-@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+@SpringBootApplication
 public class StackersApplication {
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(StackersApplication.class, args);
