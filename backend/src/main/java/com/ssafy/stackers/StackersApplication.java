5ed84ffac62af103c66b3536bc699037bf7b812d
limse0204 <limse0204@naver.com>
2023-01-30 17:43:20 +0900

    Feat: 데이터베이스 참조 어노테이션 변경

--- backend/src/main/java/com/ssafy/stackers/StackersApplication.java
+++ backend/src/main/java/com/ssafy/stackers/StackersApplication.java
@@ -2,12 +2,9 @@ package com.ssafy.stackers;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-// TODO: exclude 부분 DB 연결 후 삭제할 것
-@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+@SpringBootApplication
 public class StackersApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(StackersApplication.class, args);
     }
