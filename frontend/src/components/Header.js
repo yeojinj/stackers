0989d0fc1725320b6b7a0254aa17e116ea52647d
cjswp3550 <cjswp3550@naver.com>
2023-01-30 15:27:58 +0900

    Design: 헤더 레이아웃 구성

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,13 +1,25 @@
 import React from 'react'
+import logo from '../assets/logo.png'
+import SearchIcon from '@mui/icons-material/Search'
 import '../styles/header.css'
 
 function Header() {
   return (
     <header className="header">
       <div className="header-container">
-        <div>로고 이미지</div>
-        <div>검색창</div>
-        <div>로그인버튼 or 업로드버튼 + 프로필사진</div>
+        <img className="logo-img" src={logo}></img>
+        <div>
+          {/* 검색창 */}
+          <div className="header-search">
+            <input
+              className="search-input"
+              placeholder="검색어를 입력해주세요."
+            ></input>
+            <SearchIcon />
+          </div>
+        </div>
+        {/* 로그인버튼 or 업로드버튼 + 프로필사진 */}
+        <button className="login-btn">로그인</button>
       </div>
     </header>
   )
