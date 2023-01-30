9db24755b92d1aa5a921b62aa760e9db473e07a6
sbsc20 <sbsc20@naver.com>
2023-01-30 17:46:41 +0900

    Feat: 회원 엔티티 수정

--- backend/src/main/java/com/ssafy/stackers/model/Member.java
+++ backend/src/main/java/com/ssafy/stackers/model/Member.java
@@ -2,20 +2,28 @@ package com.ssafy.stackers.model;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
 @Getter
+@Setter
 @NoArgsConstructor
 public class Member {
-    @Id
-    @Column(name = "id", nullable = false, length = 30)
-    private String id;
 
-    @Column(name = "password", nullable = false, length = 30)
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "password", nullable = false, length = 100)
     private String password;
 
     @Column(name = "roles", nullable = false, length = 30)
@@ -24,21 +32,32 @@ public class Member {
     @Column(name = "username", nullable = false, length = 50)
     private String username;
 
-    @Column(name = "email", nullable = false, length = 30)
-    private String email;
+//    @Column(name = "nickname", nullable = false, length = 50)
+//    private String nickname;
+//
+//    @Column(name = "email", nullable = false, length = 30)
+//    private String email;
+//
+//    @Column(name = "bio", nullable = false, length = 300)
+//    private String bio;
+//
+//    @Column(name = "reg_date", nullable = false)
+//    private LocalDateTime regDate;
+//
+//    @Column(name = "last_login", nullable = false)
+//    private LocalDateTime lastLogin;
+//
+//    @Column(name = "img_path", nullable = false)
+//    private String imgPath;
+//
+//    @Column(name = "is_resign")
+//    private boolean isResign;
 
-    @Column(name = "bio", nullable = false, length = 300)
-    private String bio;
+    public List<String> getRoleList() {
+        if (this.roles.length() > 0) {
+            return Arrays.asList(this.roles.split(","));
+        }
+        return new ArrayList<>();
+    }
 
-    @Column(name = "reg_date", nullable = false)
-    private LocalDateTime regDate;
-
-    @Column(name = "last_login", nullable = false)
-    private LocalDateTime lastLogin;
-
-    @Column(name = "img_path", nullable = false)
-    private String imgPath;
-
-    @Column(name = "is_resign")
-    private boolean isResign;
 }
