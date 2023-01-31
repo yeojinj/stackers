d5898800c327caaa9dba6390346379b56c640189
limse0204 <limse0204@naver.com>
2023-01-31 11:39:34 +0900

    Feat: 비디오 저장 경로 변경

--- backend/src/main/java/com/ssafy/stackers/controller/VideoController.java
+++ backend/src/main/java/com/ssafy/stackers/controller/VideoController.java
@@ -2,6 +2,7 @@ package com.ssafy.stackers.controller;
 
 import com.ssafy.stackers.model.Video;
 import com.ssafy.stackers.service.VideoService;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.io.FilenameUtils;
 import org.apache.commons.lang3.RandomStringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,44 +12,38 @@ import org.springframework.web.multipart.MultipartFile;
 import java.io.File;
 import java.io.IOException;
 
+@Slf4j
 @RestController
 @RequestMapping("video")
 public class VideoController {
     @Autowired
-    VideoService videoService;
+    private VideoService videoService;
 
     @PostMapping("/upload")
     public String uploadVideo(@RequestPart MultipartFile file) throws IOException {
-        Video video = new Video();
-
         String sourceVideoName = file.getOriginalFilename();
         String sourceVideoNameExtension = FilenameUtils.getExtension(sourceVideoName).toLowerCase();
         FilenameUtils.removeExtension(sourceVideoName);
 
         File destinationFile;
         String destinationFileName;
-        String videoPath = "/Users/sennie/Desktop/S08P12C210/backend/src/main/resources/static/videos";
+        String videoPath = "C:\\stackers\\videos\\";          // 비디오 저장 폴더를 프로젝트 외부로 꺼내기
 
         do {
-            destinationFileName = RandomStringUtils.randomAlphanumeric(32)+"."+sourceVideoNameExtension;
+            destinationFileName =
+                RandomStringUtils.randomAlphanumeric(64) + "." + sourceVideoNameExtension;
             destinationFile = new File(videoPath + destinationFileName);
-        } while(destinationFile.exists());
+        } while (destinationFile.exists());
 
         destinationFile.getParentFile().mkdirs();
         file.transferTo(destinationFile);
 
-        video.setVideoName(destinationFileName);
-        video.setVideoOriName(sourceVideoName);
-        video.setVideoPath(videoPath);
+        Video video = Video.builder().videoName(destinationFileName).videoOriName(sourceVideoName)
+            .videoPath(videoPath).build();
 
-        System.out.println(video.getVideoName() + " " + video.getVideoPath() + " " + video.getVideoOriName());
+        log.info("[비디오 경로] {}{}", video.getVideoPath(), video.getVideoName());
         videoService.save(video);
 
-       return "SUCCESS";
-    }
-
-    @GetMapping("/hello")
-    public String test(){
-        return "hello";
+        return "video upload success";
     }
 }
