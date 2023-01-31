1fac42e824a9618ebd715bc95d569b3b6c202700
cjswp3550 <cjswp3550@naver.com>
2023-01-31 15:41:01 +0900

    Design: 스테이션 레이아웃 구성

--- frontend/src/components/station/StationListItem.js
+++ frontend/src/components/station/StationListItem.js
@@ -1,7 +1,30 @@
 import React from 'react'
+import '../../styles/stationlistitem.css'
 
 function StationListItem() {
-  return <div>스테이션 비디오</div>
+  const stationInfo = {
+    video_url: '...',
+    video_description: '비디오 설명이에요 비디오 설명이에요 비디오 설명이에요',
+    video_tags: ['#disco', '#music', '#dance', '#music', '#guitar'],
+  }
+
+  return (
+    <div className="station-item">
+      {/* 컨트롤 바 없애기 */}
+      {/* <img src={logo}></img> */}
+      <video
+        src={stationInfo.video_url}
+        controls
+        autoPlay
+        loop
+        height="287"
+        width="197"
+        style={{ borderRadius: '10px' }}
+      ></video>
+      <p className="station-description">{stationInfo.video_description}</p>
+      <div className="station-tag">{stationInfo.video_tags}</div>
+    </div>
+  )
 }
 
 export default StationListItem
