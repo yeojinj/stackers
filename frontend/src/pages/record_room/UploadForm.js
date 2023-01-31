7993749e408822374e1b3b94ed79e3db72f59276
Whitepaperone <qorwldnjs100@gmail.com>
2023-01-31 13:35:09 +0900

    Style: 브랜치 착각해서 이동중...

--- frontend/src/pages/record_room/UploadForm.js
+++ frontend/src/pages/record_room/UploadForm.js
@@ -1,29 +1,63 @@
-import Video from '../../components/Video.js';
-import './Record';
-import React from 'react';
+import Video from '../../components/Video.js'
+import './Record'
+import React from 'react'
 
 function UploadForm() {
   return (
     <div className="container">
-      <div className="left stack">
-        <Video className="item" />
-        <div className="item">노래 제목</div>
-      </div>
-      <div className="right">
-        <div className="infoForm">설명</div>
-        <div className="thumbnailForm">썸네일</div>
-        <div className="tagForm">태그</div>
-        <div className="container">
-          <div className="left">
-            <div className="instForm">연주 악기</div>
-          </div>
-          <div className="right">
-            <div className="scopeForm">공개 범위</div>
-            <button className="uploadButton">업로드</button>
+      <form onSubmit={handleSubmit}>
+        <div className="left stack">
+          <Video className="item" />
+          <div className="item">노래 제목</div>
+          <input
+            type="text"
+            name="music"
+            value={values.music}
+            onChange={handleChange}
+          ></input>
+        </div>
+        <div className="right">
+          <div className="infoForm">설명</div>
+          <input
+            type="text"
+            name="content"
+            value={values.content}
+            onChange={handleChange}
+          ></input>
+          <div className="thumbnailForm">썸네일</div>
+          <div className="tagForm">태그</div>
+          <input
+            type="text"
+            name="tagName"
+            value={values.tagName}
+            onChange={handleChange}
+          ></input>
+          <div className="container">
+            <div className="left">
+              <div className="instForm">연주 악기</div>
+              <input
+                type="text"
+                name="instName"
+                value={values.instName}
+                onChange={handleChange}
+              ></input>
+            </div>
+            <div className="right">
+              <div className="scopeForm">공개 범위</div>
+              <input
+                type="checkbox"
+                name="scope"
+                value={values.scope}
+                onChange={handleChange}
+              ></input>
+              <button type="submit" className="uploadButton">
+                업로드
+              </button>
+            </div>
           </div>
         </div>
-      </div>
+      </form>
     </div>
-  );
+  )
 }
-export default UploadForm;
+export default UploadForm
