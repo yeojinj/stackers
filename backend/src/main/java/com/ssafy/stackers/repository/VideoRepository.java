8f245713b3a326055789fe60fef26c8e4866749b
limse0204 <limse0204@naver.com>
2023-01-31 17:48:58 +0900

    Style: 코드 포맷 적용

--- backend/src/main/java/com/ssafy/stackers/repository/VideoRepository.java
+++ backend/src/main/java/com/ssafy/stackers/repository/VideoRepository.java
@@ -5,7 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-//@Repository
 public interface VideoRepository  extends JpaRepository<Video, Long> {
     Optional<Video> findById(Long id);
 }
