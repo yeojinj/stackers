a632a0e4cb7df9fdb5142982caba237a9a0e9974
Whitepaperone <qorwldnjs100@gmail.com>
2023-01-31 11:55:06 +0900

    Feat: 스택(솔로) 녹화 구현, 레코드,레코드룸 이벤트 명명

--- frontend/src/pages/record_room/Record.js
+++ frontend/src/pages/record_room/Record.js
@@ -1,15 +1,67 @@
-import React from 'react';
+import React, { useRef, useState, useEffect } from 'react';
 import './Record.css';
+import { ReactMediaRecorder } from 'react-media-recorder';
+
+const VideoPreview = ({ stream }) => {
+  const videoRef = useRef(null);
+
+  useEffect(() => {
+    if (videoRef.current && stream) {
+      videoRef.current.srcObject = stream;
+    }
+  }, [stream]);
+  if (!stream) {
+    return null;
+  }
+  return <video ref={videoRef} width={500} height={500} autoPlay controls />;
+};
+
 function Record(props) {
+  const [enable, setEnable] = useState(true);
   return (
-    <div className="stack">
-      <div className="record">RECORD</div>
-      <div className="container">
-        <div className="box">timer</div>
-        <div className="box">stream</div>
-        <div className="box">time</div>
-      </div>
-      <button>start record</button>
+    <div className="record">
+      <ReactMediaRecorder
+        video
+        blobPropertyBag={{
+          type: 'video/mp4',
+        }}
+        render={({
+          previewStream,
+          status,
+          startRecording,
+          stopRecording,
+          mediaBlobUrl,
+        }) => {
+          return (
+            <div>
+              <p>{status}</p>
+              <div className="box">
+                <button
+                  onClick={() => {
+                    startRecording();
+                    setTimeout(stopRecording, 60000);
+                  }}
+                >
+                  Start Recording
+                </button>
+                <button onClick={stopRecording}>Stop Recording</button>
+                <button
+                  onClick={() => {
+                    startRecording();
+                    setTimeout(stopRecording, 60000);
+                    setEnable(!enable);
+                  }}
+                >
+                  togglestreaming
+                </button>
+              </div>
+              {/* <audio src={mediaBlobUrl} controls autoPlay loop /> */}
+              <video src={mediaBlobUrl} controls />
+              {enable && <VideoPreview stream={previewStream} />}
+            </div>
+          );
+        }}
+      />
     </div>
   );
 }
--- frontend/src/pages/record_room/RecordRoom.js
+++ frontend/src/pages/record_room/RecordRoom.js
@@ -1,19 +1,39 @@
-import Record from './Record.js';
-import React from 'react';
-import StackUploadModal from './StackUploadModal';
-import LightIcon from '@mui/icons-material/Light';
-import PhotoCameraFrontIcon from '@mui/icons-material/PhotoCameraFront';
-import InfoOutlinedIcon from '@mui/icons-material/InfoOutlined';
+import Record from './Record.js'
+import React, { useState } from 'react'
+import StackUploadModal from './StackUploadModal'
+import LightIcon from '@mui/icons-material/Light'
+import PhotoCameraFrontIcon from '@mui/icons-material/PhotoCameraFront'
+import InfoOutlinedIcon from '@mui/icons-material/InfoOutlined'
+import Modal from '@mui/material/Modal'
 
 function RecordRoom() {
+  // const navigate = useNavigate()
+
+  const goBack = () => {
+    // // 이전 페이지로 이동
+    // navigate(-1)
+  }
+  const [open, setOpen] = useState(false)
+  const handleOpen = () => {
+    setOpen(true)
+  }
+
+  const handleClose = () => {
+    setOpen(false)
+  }
+  const setNoneStack = () => {}
+  const showToolTip = () => {}
   return (
     <div>
       <LightIcon></LightIcon>
       <div className="container">
         <div className="stack">
           <p className="box"></p>
-          <PhotoCameraFrontIcon className="box"></PhotoCameraFrontIcon>
-          <InfoOutlinedIcon className="box" />
+          <PhotoCameraFrontIcon
+            className="box"
+            onClick={setNoneStack}
+          ></PhotoCameraFrontIcon>
+          <InfoOutlinedIcon className="box" onClick={showToolTip} />
         </div>
         <div className="box">
           <Record />
@@ -22,13 +42,17 @@ function RecordRoom() {
           <p></p>
           <p></p>
           <div className="box">
-            <button className="box">취소</button>
-            <button className="box">업로드</button>
+            <button className="box" onClick={goBack}>
+              취소
+            </button>
+            <button onClick={handleOpen}>업로드</button>
+            <Modal open={open} onClose={handleClose}>
+              <StackUploadModal />
+            </Modal>
           </div>
         </div>
       </div>
-      <StackUploadModal />
     </div>
-  );
+  )
 }
-export default RecordRoom;
+export default RecordRoom
