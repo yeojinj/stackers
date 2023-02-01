e7972bc1152ccf71e7dd0b5d79590a4dfbeb5af0
이창민 <dlckdals1004@naver.com>
2023-02-01 17:48:53 +0900

    Feat: Login 모달 제작중

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import MainRoom from './pages/main_room/MainRoom'
 import RecordRoom from './pages/record_room/RecordRoom'
 import StationRoom from './pages/station_room/stationview/StationRoom'
+import LogIn from './pages/sign_folder/LogIn'
 import { Link, Route, Routes } from 'react-router-dom'
 
 function App() {
@@ -17,11 +18,15 @@ function App() {
         <li>
           <Link to="/StationRoom">Station Room</Link>
         </li>
+        <li>
+          <Link to="/LogIn">LogIn</Link>
+        </li>
       </ul>
       <Routes>
         <Route path="/MainRoom" element={<MainRoom />} />
         <Route path="/RecordRoom" element={<RecordRoom />} />
         <Route path="/StationRoom" element={<StationRoom />} />
+        <Route path="/LogIn" element={<LogIn />} />
       </Routes>
     </div>
   )
