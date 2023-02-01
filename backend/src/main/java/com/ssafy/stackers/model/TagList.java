dff3c452a73ea624a2ec059a923859fdf0ee6469
limse0204 <limse0204@naver.com>
2023-02-01 17:50:39 +0900

    Feat: 태그리스트 엔티티 생성

---
+++ backend/src/main/java/com/ssafy/stackers/model/TagList.java
@@ -0,0 +1,26 @@
+package com.ssafy.stackers.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class TagList {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "tag_id")
+    private Tag tag;
+
+    @ManyToOne
+    @JoinColumn(name = "station_id")
+    private Station station;
+}
