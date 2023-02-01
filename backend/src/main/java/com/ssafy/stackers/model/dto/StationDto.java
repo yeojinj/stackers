70ecee0888d9766e02ac814fd21d9a3087fe7878
limse0204 <limse0204@naver.com>
2023-02-01 17:09:04 +0900

    Feat: 스테이션 Dto 수정

--- backend/src/main/java/com/ssafy/stackers/model/dto/StationDto.java
+++ backend/src/main/java/com/ssafy/stackers/model/dto/StationDto.java
@@ -7,4 +7,11 @@ public class StationDto {
     private String content;
     private String music;
     private Long instrumentId;
+    private int heartCnt;
+    private int remainDepth;
+    private boolean isPublic;
+    private boolean isComplete;
+    private boolean isDelete;
+
+
 }
