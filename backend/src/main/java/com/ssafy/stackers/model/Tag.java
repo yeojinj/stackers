63ef2ba53f02f1e537b5d8236e1dbced97fa778b
limse0204 <limse0204@naver.com>
2023-02-01 17:50:45 +0900

    Feat: 태그 엔티티 생성

---
+++ backend/src/main/java/com/ssafy/stackers/model/Tag.java
@@ -0,0 +1,28 @@
+package com.ssafy.stackers.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Tag {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 50)
+    private String name;
+
+    @Builder
+    public Tag(String name) {
+        this.name = name;
+    }
+}
