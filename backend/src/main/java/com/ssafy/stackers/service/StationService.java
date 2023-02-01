9104a10ab17a5ebf1a3c0bc9b2bdf9c5c64dc7bb
limse0204 <limse0204@naver.com>
2023-02-01 14:43:58 +0900

    Feat: 스테이션 저장 함수 수정

--- backend/src/main/java/com/ssafy/stackers/service/StationService.java
+++ backend/src/main/java/com/ssafy/stackers/service/StationService.java
@@ -11,13 +11,21 @@ import org.springframework.transaction.annotation.Transactional;
 @Service
 @Transactional(readOnly = true)
 public class StationService {
+
     @Autowired
     private StationRepository stationRepository;
 
     @Transactional
-    public void saveWithMember(Station station, Video video) {
-        Station s = Station.builder().content(station.getContent()).music(station.getMusic())
-            .regTime(station.getRegTime()).remainDepth(3).isPublic(true).video(video).build();
+    public void saveWithMemberAndVideo(Station station, Video video, Member member) {
+        Station s = Station.builder()
+            .content(station.getContent())
+            .music(station.getMusic())
+            .heartCnt(0)
+            .remainDepth(3)
+            .isPublic(true)
+            .member(member)
+            .video(video)
+            .build();
 
         stationRepository.save(s);
     }
