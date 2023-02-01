7bff0cdf72ce2f97f51993bb26671c9f28c697e5
limse0204 <limse0204@naver.com>
2023-02-01 14:43:39 +0900

    Feat: 비디오 업로드 함수 추가

--- backend/src/main/java/com/ssafy/stackers/service/VideoService.java
+++ backend/src/main/java/com/ssafy/stackers/service/VideoService.java
@@ -8,16 +8,19 @@ import net.bramp.ffmpeg.FFmpeg;
 import net.bramp.ffmpeg.FFmpegExecutor;
 import net.bramp.ffmpeg.FFprobe;
 import net.bramp.ffmpeg.builder.FFmpegBuilder;
+import org.apache.commons.io.FilenameUtils;
+import org.apache.commons.lang3.RandomStringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
 
 @Service
 @Transactional(readOnly = true)
 public class VideoService {
 
     @Autowired
-    VideoRepository videoRepository;
+    private VideoRepository videoRepository;
 
     @Transactional
     public void save(Video video) {
@@ -25,6 +28,38 @@ public class VideoService {
             .videoOriName(video.getVideoOriName()).videoPath(video.getVideoPath()).build();
         videoRepository.save(v);
     }
+
+    /**
+     * 데이터베이스에 비디오 업로드
+     * */
+    public Video uploadVideo(MultipartFile file) throws IOException {
+        String sourceVideoName = file.getOriginalFilename();
+        String sourceVideoNameExtension = FilenameUtils.getExtension(sourceVideoName).toLowerCase();
+        FilenameUtils.removeExtension(sourceVideoName);
+
+        File destinationFile;
+        String destinationFileName;
+        String videoPath = "C:\\stackers\\videos\\";
+//        String videoPath = "/Users/sennie/stackers/videos";
+
+        do {
+            destinationFileName =
+                RandomStringUtils.randomAlphanumeric(64) + "." + sourceVideoNameExtension;
+            destinationFile = new File(videoPath + destinationFileName);
+        } while (destinationFile.exists());
+
+        destinationFile.getParentFile().mkdirs();
+        file.transferTo(destinationFile);
+
+        Video video = Video.builder().videoName(destinationFileName).videoOriName(sourceVideoName)
+            .videoPath(videoPath).build();
+
+        return video;
+    }
+
+    /**
+     * 썸네일 추출 함수
+     */
 
     public void exportThumbnail() throws IOException {
         // 영상 파일 경로
@@ -39,13 +74,11 @@ public class VideoService {
         FFprobe ffprobe = new FFprobe(ffmpegPath + "ffprobe");
 
         // 썸네일 추출 명령어
-        FFmpegBuilder builder = new FFmpegBuilder()
-            .overrideOutputFiles(true)          // 썸네일 파일 존재할 경우 덮어쓰기
+        FFmpegBuilder builder = new FFmpegBuilder().overrideOutputFiles(
+                true)          // 썸네일 파일 존재할 경우 덮어쓰기
             .setInput(videoPath)                // 영상 파일 경로
             .addOutput(thumbnailPath + "thumb.png")     // 썸네일 추출 절대 경로
-
-            .setFrames(1)
-            .done();
+            .setFrames(1).done();
 
         FFmpegExecutor executor = new FFmpegExecutor(ffmpeg,
             ffprobe);        // FFmpeg 명령어 실행을 위한 FFmpegExecutor 객체 생성
