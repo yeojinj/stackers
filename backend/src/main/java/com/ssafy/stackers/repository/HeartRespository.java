471cebcf8d1bfff86035a78cb9fa6c828b9c24c8
limse0204 <limse0204@naver.com>
2023-02-01 17:07:52 +0900

    Feat: 좋아요 레포지토리 생성

---
+++ backend/src/main/java/com/ssafy/stackers/repository/HeartRespository.java
@@ -0,0 +1,8 @@
+package com.ssafy.stackers.repository;
+
+import com.ssafy.stackers.model.Heart;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface HeartRespository extends JpaRepository<Heart, Long> {
+
+}
