ce2fd6528259481c3f13b7ae66419be975fc5f06
limse0204 <limse0204@naver.com>
2023-02-01 14:43:23 +0900

    Feat: 연주자 엔티티 생성

--- backend/src/main/java/com/ssafy/stackers/model/StackerInfo.java
+++ backend/src/main/java/com/ssafy/stackers/model/StackerInfo.java
@@ -2,11 +2,13 @@ package com.ssafy.stackers.model;
 
 import jakarta.persistence.*;
 import lombok.AccessLevel;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
+@Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class Stackers {
+public class StackerInfo {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -14,6 +16,4 @@ public class Stackers {
     @ManyToOne
     @JoinColumn(name = "station_id")
     private Station station;
-
-
 }
