75c212ec921af32fb497e37258287001a1e0b574
limse0204 <limse0204@naver.com>
2023-02-02 14:33:51 +0900

    Feat: findBy 에러 처리

--- backend/src/main/java/com/ssafy/stackers/service/InstrumentService.java
+++ backend/src/main/java/com/ssafy/stackers/service/InstrumentService.java
@@ -1,7 +1,10 @@
 package com.ssafy.stackers.service;
 
+import com.ssafy.stackers.exception.CustomException;
 import com.ssafy.stackers.model.Instrument;
+import com.ssafy.stackers.model.Station;
 import com.ssafy.stackers.repository.InstrumentRepository;
+import com.ssafy.stackers.utils.error.ErrorCode;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -19,7 +22,10 @@ public class InstrumentService {
     }
 
     public Instrument findById(Long id){
-        return instrumentRepository.findById(id).get();
+        Instrument instrument = instrumentRepository.findById(id).orElseThrow(() -> new CustomException(
+            ErrorCode.ENTITY_NOT_FOUND));
+
+        return instrument;
     }
 
 }
--- backend/src/main/java/com/ssafy/stackers/service/TagService.java
+++ backend/src/main/java/com/ssafy/stackers/service/TagService.java
@@ -1,21 +1,46 @@
 package com.ssafy.stackers.service;
 
+import com.ssafy.stackers.exception.CustomException;
 import com.ssafy.stackers.model.Tag;
 import com.ssafy.stackers.repository.TagRepository;
+import com.ssafy.stackers.utils.error.ErrorCode;
+import java.util.ArrayList;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
 @Service
 @Transactional(readOnly = true)
 public class TagService {
+
     @Autowired
     private TagRepository tagRepository;
+
     @Transactional
-    public void save(List<String> tags){
-        for(int i = 0; i < tags.size(); i++){
-            Tag newTag = Tag.builder().name(tags.get(i)).build();
-            tagRepository.save(newTag);
+    public List<Tag> save(List<String> tags) {
+        // 태그 정보 반환 리스트 생성
+        List<Tag> tagList = new ArrayList<>();
+        Tag newTag = null;
+
+        for (int i = 0; i < tags.size(); i++) {
+            if(!tagRepository.existsByName(tags.get(i))){
+                //	태그가 없으면 -> DB 저장 -> List에 Tag 추가
+                newTag = Tag.builder().name(tags.get(i)).build();
+                tagRepository.save(newTag);
+            } else{
+                newTag = tagRepository.findByName(tags.get(i)).orElseThrow(() -> new CustomException(ErrorCode.ENTITY_NOT_FOUND));
+            }
+            tagList.add(newTag);
         }
+
+        return tagList;
+    }
+
+    public Tag findById(Long id) {
+        Tag tag = tagRepository.findById(id)
+            .orElseThrow(() -> new CustomException(ErrorCode.ENTITY_NOT_FOUND));
+        return tag;
     }
 }
