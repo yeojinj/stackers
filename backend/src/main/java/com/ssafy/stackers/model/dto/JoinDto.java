9c03d5d8e176f891b0e3b118dc55e2dc18b1dd02
sbsc20 <sbsc20@naver.com>
2023-02-02 11:48:11 +0900

    Feat: 회원 엔티티 필드 추가

---
+++ backend/src/main/java/com/ssafy/stackers/model/dto/JoinDto.java
@@ -0,0 +1,12 @@
+package com.ssafy.stackers.model.dto;
+
+import lombok.Getter;
+
+@Getter
+public class JoinDto {
+
+    private String username;
+    private String password;
+    private String email;
+
+}
