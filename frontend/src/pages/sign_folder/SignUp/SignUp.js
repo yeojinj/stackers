aa6a473f79d03585f07c6bf1faaa67f4d90b070d
이창민 <dlckdals1004@naver.com>
2023-02-03 09:46:50 +0900

    Design: SignUp 디자인

--- frontend/src/pages/sign_folder/SignUp/SignUp.js
+++ frontend/src/pages/sign_folder/SignUp/SignUp.js
@@ -1,17 +1,22 @@
 import React from 'react'
+import LogoImage from './LogoImage.png'
+import LogoText from './LogoText.png'
+import './SignUp.css'
 
 function SignUp() {
   return (
     <div>
-      <p>안녕</p>
-      <div
-        style={{
-          width: '400px',
-          height: '400px',
-          border: '1px solid black',
-          margin: 'auto'
-        }}
-      ></div>
+      <div className="SignUp">
+        <div style={{ margin: 'auto' }}>
+          <p>
+            <img src={LogoImage} alt="LogoImage" style={{ margin: 'auto' }} />
+          </p>
+          <p style={{ display: 'flex' }}>
+            <img src={LogoText} alt="LogoText" />
+            <h3 style={{ margin: '0px' }}>에 오신 것을 환영합니다.</h3>
+          </p>
+        </div>
+      </div>
     </div>
   )
 }
