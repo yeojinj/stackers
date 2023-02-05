8d612ab04c676ddc113a1edfdc811586efa019ea
LIM SEEUN <ssen.lim@gmail.com>
2023-02-06 07:50:02 +0900

    Feat: 스테이션 응답 DTO 타입 수정

--- backend/src/main/java/com/ssafy/stackers/model/dto/StationDto.java
+++ backend/src/main/java/com/ssafy/stackers/model/dto/StationDto.java
@@ -2,16 +2,18 @@ package com.ssafy.stackers.model.dto;
 
 import java.util.List;
 import lombok.Getter;
+import lombok.ToString;
 
 @Getter
+@ToString
 public class StationDto {
     private String content;
     private String music;
     private Long instrumentId;
     private int heartCnt;
     private int remainDepth;
-    private boolean isPublic;
-    private boolean isComplete;
+    private int isPublic;
+    private int isComplete;
     private boolean isDelete;
     private List<String> tags;
     private Long prevStationId;
