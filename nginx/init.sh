fde54f12787783d1c7a6e32aad183f7156ee7b06
sbsc20 <sbsc20@naver.com>
2023-02-06 13:06:29 +0900

    Feat: nginx 설정 파일

--- nginx/init.sh
+++ nginx/init.sh
@@ -6,9 +6,4 @@ mv /etc/nginx/test.conf /etc/nginx/sites-available/test.conf
 ln -s /etc/nginx/sites-available/test.conf /etc/nginx/sites-enabled/test.conf
 
 mv /etc/nginx/default.conf /etc/nginx/conf.d/default.conf
-
-sed '$ d' -i /etc/nginx/nginx.conf
-echo "    include /etc/nginx/sites-enabled/*.conf;" >> /etc/nginx/nginx.conf
-echo "    server_names_hash_bucket_size 64;" >> /etc/nginx/nginx.conf
-echo "}" >> /etc/nginx/nginx.conf
-
+mv /etc/nginx/nginx.conf /etc/nginx/nginx.conf
