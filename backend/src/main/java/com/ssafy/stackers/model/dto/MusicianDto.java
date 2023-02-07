622030680c2d9e8a3d0c751f44a12f964b812714
limse0204 <limse0204@naver.com>
2023-02-07 17:17:35 +0900

    Feat: 연주자 DTO 생성자 추가

--- backend/src/main/java/com/ssafy/stackers/model/dto/MusicianDto.java
+++ backend/src/main/java/com/ssafy/stackers/model/dto/MusicianDto.java
@@ -15,6 +15,13 @@ public class MusicianDto {
     private String instrumentName;  // 악기명
     private String musicianUsername;    // 연주자 아이디
     private String musicianImgPath;     // 연주자 프로필 사진 경로
-    private String musicionImgName;     // 연주자 프로필 사진 파일명
+    private String musicianImgName;     // 연주자 프로필 사진 파일명
 
+    public MusicianDto(Long instrumentId, String instrumentName, String musicianUsername,
+        String musicianImgPath) {
+        this.instrumentId = instrumentId;
+        this.instrumentName = instrumentName;
+        this.musicianUsername = musicianUsername;
+        this.musicianImgPath = musicianImgPath;
+    }
 }
