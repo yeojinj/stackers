2ffc6126f88abccb21d81734568960d0ca7d00d0
limse0204 <limse0204@naver.com>
2023-02-07 17:17:10 +0900

    Feat: 유저 DTO 변경

--- backend/src/main/java/com/ssafy/stackers/model/dto/LoginMemberDto.java
+++ backend/src/main/java/com/ssafy/stackers/model/dto/LoginMemberDto.java
@@ -10,11 +10,17 @@ import lombok.Getter;
 public class LoginMemberDto {
 
 //    아이디, 닉네임, 이메일, 소개(바이오), 프로필 사진, 악기, 소속
+    private Long id;
+    private String username;
+    private String nickname;
+    private String email;
+    private String bio;
+    private String imgPath;
 
-    String username;
-    String nickname;
-    String email;
-    String bio;
-    String imgPath;
-
+    public LoginMemberDto(Long id, String username, String nickname, String imgPath) {
+        this.id = id;
+        this.username = username;
+        this.nickname = nickname;
+        this.imgPath = imgPath;
+    }
 }
