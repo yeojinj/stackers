122b3200cbec19eff13c4d69b5827d4194e605e0
yogurt05j <yogurt05j@gmail.com>
2023-02-08 17:42:39 +0900

    Feat: 검색용 SearchController 추가

---
+++ backend/src/main/java/com/ssafy/stackers/controller/SearchController.java
@@ -0,0 +1,28 @@
+package com.ssafy.stackers.controller;
+
+import com.ssafy.stackers.model.dto.MainStationDto;
+import com.ssafy.stackers.model.dto.SearchResultDto;
+import com.ssafy.stackers.service.SearchService;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import java.util.List;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@Tag(name = "Search", description = "검색 관련 API")
+@RestController
+@RequestMapping("api/search")
+public class SearchController {
+    @Autowired
+    private SearchService searchService;
+
+    @GetMapping("/{keyword}")
+    public SearchResultDto search(@PathVariable("keyword") String keyword){
+        System.out.println("===================search===================");
+        return searchService.searchStation(keyword);
+    }
+}
