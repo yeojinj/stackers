6d7180bd83b522d74bd6fe58b8fba7cc9435e67c
yogurt05j@gmail.com <yogurt05j@gmail.com>
2023-02-10 11:25:08 +0900

    Feat: 동영상 썸네일 및 메타데이터 추출 기능 삭제

--- backend/src/main/java/com/ssafy/stackers/service/VideoService.java
+++ backend/src/main/java/com/ssafy/stackers/service/VideoService.java
@@ -143,58 +143,6 @@ public class VideoService {
     }
 
     /**
-     * 동영상 썸네일 추출
-     * 동영상 파일 경로, 썸네일 추출 경로 (프로젝트 외부) 절대 경로로 지정함
-     */
-    public void exportThumbnail() throws IOException {
-        // 영상 파일 경로
-        String videoPath = "C:\\test\\videos\\test.mp4";
-
-        // 썸네일 추출 절대 경로
-        String thumbnailPath = "C:\\test\\thumbs\\";
-
-        // ffmpeg 설치 파일 경로 -> 환경 변수로 설정
-        FFmpeg ffmpeg = new FFmpeg("ffmpeg");
-        FFprobe ffprobe = new FFprobe("ffprobe");
-
-        // 썸네일 추출 명령어
-        FFmpegBuilder builder = new FFmpegBuilder()
-            .setInput(videoPath)                // 영상 파일 경로
-            .overrideOutputFiles(true)          // 썸네일 파일 존재할 경우 덮어쓰기
-            .addOutput(thumbnailPath + "thumb.png")   // 썸네일 추출 절대 경로
-            .addExtraArgs("-ss", "00:00:01")            // 썸네일 추출 시작점 ([영상 길이 / 10]으로 10개 뽑을 예정)
-            .setFrames(1)
-            .done();
-
-        FFmpegExecutor executor = new FFmpegExecutor(ffmpeg,
-            ffprobe);        // FFmpeg 명령어 실행을 위한 FFmpegExecutor 객체 생성
-        executor.createJob(builder).run();                                    // one-pass encodes
-    }
-
-    /**
-     * 동영상 메타데이터 추출
-     */
-    public void getMetaData() throws IOException {
-        // 영상 파일 경로
-        String videoPath = "C:\\test\\videos\\test.mp4";
-
-        // ffmpeg 설치 파일 경로 -> 환경 변수로 설정
-        FFmpeg ffmpeg = new FFmpeg("ffmpeg");
-        FFprobe ffprobe = new FFprobe("ffprobe");
-
-        FFmpegProbeResult probeResult = ffprobe.probe(videoPath);
-
-        log.info("===== Video Meta Data =====");
-        log.info("duration: " + probeResult.getStreams().get(0).duration);
-        log.info("codec name: " + probeResult.getStreams().get(0).codec_name);
-        log.info("width: " + probeResult.getStreams().get(0).width);
-        log.info("height: " + probeResult.getStreams().get(0).height);
-        log.info("bit_rate: " + probeResult.getStreams().get(0).bit_rate);
-        log.info("avg_frame_rate: " + probeResult.getStreams().get(0).avg_frame_rate);
-
-    }
-
-    /**
      * 동영상 인코딩
      */
     public String encodeVideo(String localPath) throws IOException {
