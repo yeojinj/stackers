83d5bccad7581f8bba6ba6c3978f335fd65c1687
limse0204 <limse0204@naver.com>
2023-02-16 21:32:34 +0900

    Design: 업로딩 디자인 수정

--- frontend/src/pages/record_room/UploadLoading.js
+++ frontend/src/pages/record_room/UploadLoading.js
@@ -16,6 +16,8 @@ function UpladLoading() {
       navigate(`/MyPage/${username}`)
     }
   }
+
+  console.log(moveToMain)
   setTimeout(() => {
     moveToMain(true)
   }, 4000)
@@ -27,10 +29,11 @@ function UpladLoading() {
       <Header />
       <div className="loading-conatiner">
         <p className="stacking">스택을 쌓고 있어요</p>
-
-        <div className="stack-box" id="stack-box1"></div>
-        <div className="stack-box" id="stack-box2"></div>
-        <div className="stack-box" id="stack-box3"></div>
+        <div id="uploading-divs">
+          <div className="uploading-div yellow"></div>
+          <div className="uploading-div red"></div>
+          <div className="uploading-div blue"></div>
+        </div>
       </div>
     </>
   )
